9ed29a7f90ede2c68d77b68129a1a3efec3593eb
BeneDev <seriousbenedikt@web.de>
2018-05-17 14:55:30 +0200

    Some button examples

--- Assets/Scripts/SimpleMenu.cs
+++ Assets/Scripts/SimpleMenu.cs
@@ -1,19 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SimpleMenu : Menu<SimpleMenu> {
 
     //TODO make this abstract and then derive menus from here
 
-	public static void Show()
+    [SerializeField] private Button button;
+
+    public static void Show()
     {
         Open();
+
+        Instance.button.onClick.AddListener(delegate { Debug.Log("Clicked"); });
     }
 
     public static void Hide()
     {
         Close();
+    }
+
+    public void OpenOptions()
+    {
+        OptionsMenu.Show();
     }
 
 }
